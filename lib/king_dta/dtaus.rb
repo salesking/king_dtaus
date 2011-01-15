1b50da2d18d549bd6164486659166f6f6765d605
Kim Rudolph <mail@kimrudolph.de>
2011-01-15 13:19:52 +0100

    ruby 1.9.x enabled

--- lib/king_dta/dtaus.rb
+++ lib/king_dta/dtaus.rb
@@ -1,3 +1,4 @@
+# encoding: utf-8
 #Erstelle eine DTAUS-Datei.
 #Typ ist 'LK' (Lastschrift Kunde) oder 'GK' (Gutschrift Kunde)
 #
@@ -16,13 +17,13 @@ module KingDta
       raise "Wrong date format. Make it a Time or Date object with yyyy-mm-dd" unless date.respond_to?(:strftime)
       raise "Unknown order type: #{typ}. Allowed Values are LK, GK" unless ['LK','GK'].include?(typ)
       @date = date
-      @typ	= typ
-      @value_pos  = true	#all values are positive by default. Variable changes with first booking entry
+      @typ  = typ
+      @value_pos  = true  #all values are positive by default. Variable changes with first booking entry
       @closed     = false
       @default_text = '' # default verwendungzweck
     end
-    
-    #	Set the sending account(you own)
+
+    #  Set the sending account(you own)
     # === Parameter
     # account<Account>:: the sending account, must be an instance of class
     # KingDta::Account
@@ -53,7 +54,7 @@ module KingDta
     def add ( booking )
       raise "The file has alreay been closed, cannot add new booking" if @closed
       #the first booking decides wether all values are po or negative
-      @value_pos = booking.pos?	if bookings.empty?
+      @value_pos = booking.pos?  if bookings.empty?
       raise "The prefix within bookings changed from #{@value_pos} to #{booking.pos?}" if @value_pos != booking.pos?
       bookings << booking
     end
@@ -74,9 +75,9 @@ module KingDta
     end
 
     def set_checksums
-      @sum_bank_account_numbers, @sum_bank_numbers, @sum_values	= 0,0,0
+      @sum_bank_account_numbers, @sum_bank_numbers, @sum_values  = 0,0,0
       bookings.each do |b|
-        @sum_bank_account_numbers	+= b.account.bank_account_number
+        @sum_bank_account_numbers  += b.account.bank_account_number
         @sum_bank_numbers += b.account.bank_number
         @sum_values += b.value
       end
@@ -87,7 +88,7 @@ module KingDta
     # filename<String>:: defaults to DTAUS0.TXT
     def create_file(filename ='DTAUS0.TXT')
       file = open( filename, 'w')
-      file	<< create
+      file  << create
       file.close()
       print "#{filename} created containing #{@bookings.size} bookings\n"
     end
@@ -95,41 +96,41 @@ module KingDta
 
     #Erstellen A-Segment der DTAUS-Datei
     #Aufbau des Segments:
-    # Nr.	Start	Länge 		Beschreibung
-    # 1 	0  		4 Zeichen  	Länge des Datensatzes, immer 128 Bytes, also immer "0128"
-    # 2 	4  		1 Zeichen  	Datensatz-Typ, immer 'A'
-    # 3 	5  		2 Zeichen  	Art der Transaktionen
-    #						"LB" für Lastschriften Bankseitig
-    #						"LK" für Lastschriften Kundenseitig
-    #						"GB" für Gutschriften Bankseitig
-    #						"GK" für Gutschriften Kundenseitig
-    # 4 	7  		8 Zeichen  	Bankleitzahl des Auftraggebers
-    # 5 	15  	8 Zeichen  	CST, "00000000", nur belegt, wenn Diskettenabsender Kreditinstitut
-    # 6 	23  	27 Zeichen  Name des Auftraggebers
-    # 7 	50  	6 Zeichen  	aktuelles Datum im Format DDMMJJ
-    # 8 	56  	4 Zeichen  	CST, "    " (Blanks)
-    # 9 	60  	10 Zeichen  Kontonummer des Auftraggebers
-    # 10 	70  	10 Zeichen  Optionale Referenznummer
-    # 11a 80  	15 Zeichen  Reserviert, 15 Blanks
-    # 11b 95  	8 Zeichen  	Ausführungsdatum im Format DDMMJJJJ. Nicht jünger als Erstellungsdatum (A7), jedoch höchstens 15 Kalendertage später. Sonst Blanks.
-    # 11c 103  	24 Zeichen  Reserviert, 24 Blanks
-    # 12 	127  	1 Zeichen  	Währungskennzeichen
-    #						" " = DM
-    #						"1" = Euro
+    # Nr.  Start  Länge     Beschreibung
+    # 1   0      4 Zeichen    Länge des Datensatzes, immer 128 Bytes, also immer "0128"
+    # 2   4      1 Zeichen    Datensatz-Typ, immer 'A'
+    # 3   5      2 Zeichen    Art der Transaktionen
+    #            "LB" für Lastschriften Bankseitig
+    #            "LK" für Lastschriften Kundenseitig
+    #            "GB" für Gutschriften Bankseitig
+    #            "GK" für Gutschriften Kundenseitig
+    # 4   7      8 Zeichen    Bankleitzahl des Auftraggebers
+    # 5   15    8 Zeichen    CST, "00000000", nur belegt, wenn Diskettenabsender Kreditinstitut
+    # 6   23    27 Zeichen  Name des Auftraggebers
+    # 7   50    6 Zeichen    aktuelles Datum im Format DDMMJJ
+    # 8   56    4 Zeichen    CST, "    " (Blanks)
+    # 9   60    10 Zeichen  Kontonummer des Auftraggebers
+    # 10   70    10 Zeichen  Optionale Referenznummer
+    # 11a 80    15 Zeichen  Reserviert, 15 Blanks
+    # 11b 95    8 Zeichen    Ausführungsdatum im Format DDMMJJJJ. Nicht jünger als Erstellungsdatum (A7), jedoch höchstens 15 Kalendertage später. Sonst Blanks.
+    # 11c 103    24 Zeichen  Reserviert, 24 Blanks
+    # 12   127    1 Zeichen    Währungskennzeichen
+    #            " " = DM
+    #            "1" = Euro
     #    Insgesamt 128 Zeichen
     def add_a
       data = '0128'
-      data += 'A'				#Segment
-      data += @typ				#Lastschriften Kunde
-      data += '%8i' % @account.bank_number #.rjust(8)	#bank_number
+      data += 'A'        #Segment
+      data += @typ        #Lastschriften Kunde
+      data += '%8i' % @account.bank_number #.rjust(8)  #bank_number
       data += '%08i' % 0                 #belegt, wenn Bank
       data += '%-27.27s' % @account.owner
-      data += @date.strftime("%d%m%y")	#aktuelles Datum im Format DDMMJJ
+      data += @date.strftime("%d%m%y")  #aktuelles Datum im Format DDMMJJ
       data += ' ' * 4  #bankinternes Feld
       data += '%010i' % @account.bank_account_number
       data += '%010i' % 0 #Referenznummer
       data += ' '  * 15  #Reserve
-      data += '%8s' % @date.strftime("%d%m%Y")	   #Ausführungsdatum (ja hier 8 Stellen, Erzeugungsdat. hat 6 Stellen)
+      data += '%8s' % @date.strftime("%d%m%Y")     #Ausführungsdatum (ja hier 8 Stellen, Erzeugungsdat. hat 6 Stellen)
       data += ' ' * 24   #Reserve
       data += '1'   #Kennzeichen Euro
       raise "DTAUS: Längenfehler A (#{data.size} <> 128)\n" if data.size != 128
@@ -138,54 +139,54 @@ module KingDta
 
     #Erstellen C-Segmente (Buchungen mit Texten) der DTAUS-Datei
     #Aufbau:
-    #	Nr.	St	Länge		Beschreibung
-    #	1 	0  	4 Zeichen  	Länge des Datensatzes, 187 + x * 29 (x..Anzahl Erweiterungsteile)
-    #	2 	4  	1 Zeichen  	Datensatz-Typ, immer 'C'
-    #	3 	5  	8 Zeichen  	Bankleitzahl des Auftraggebers (optional)
-    #	4 	13  8 Zeichen  	Bankleitzahl des Kunden
-    #	5 	21  10 Zeichen  Kontonummer des Kunden
-    #	6 	31  13 Zeichen  Verschiedenes
-    #			1. Zeichen: "0"
-    #			2. - 12. Zeichen: interne Kundennummer oder Nullen
-    #			13. Zeichen: "0"
-    #			Die interne Nummer wird vom erstbeauftragten Institut zum endbegünstigten Institut weitergeleitet. Die Weitergabe der internenen Nummer an den Überweisungsempfänger ist der Zahlstelle freigestellt.
-    #	7 	44  5 Zeichen  Art der Transaktion (7a: 2 Zeichen, 7b: 3 Zeichen)
-    #			"04000" Lastschrift des Abbuchungsauftragsverfahren
-    #			"05000" Lastschrift des Einzugsermächtigungsverfahren
-    #			"05005" Lastschrift aus Verfügung im elektronischen Cash-System
-    #			"05006" Wie 05005 mit ausländischen Karten
-    #			"51000" Überweisungs-Gutschrift
-    #			"53000" Überweisung Lohn/Gehalt/Rente
-    #			"5400J" Vermögenswirksame Leistung (VL) ohne Sparzulage
-    #			"5400J" Vermögenswirksame Leistung (VL) mit Sparzulage
-    #			"56000" Überweisung öffentlicher Kassen
-    #			Die im Textschlüssel mit J bezeichnete Stelle, wird bei Übernahme in eine Zahlung automatisch mit der jeweils aktuellen Jahresendziffer (7, wenn 97) ersetzt.
-    #	8 	49  1 Zeichen  Reserviert, " " (Blank)
-    #	9 	50  11 Zeichen  Betrag in DM
-    #	10 	61  8 Zeichen  Bankleitzahl des Auftraggebers
-    #	11 	69  10 Zeichen  Kontonummer des Auftraggebers
-    #	12 	79  11 Zeichen  Betrag in Euro einschließlich Nachkommastellen, nur belegt, wenn Euro als Währung angegeben wurde (A12, C17a), sonst Nullen
-    #	13 	90  3 Zeichen  Reserviert, 3 Blanks
-    #	14a 93  27 Zeichen  Name des Kunden
-    #	14b 120  8 Zeichen  Reserviert, 8 Blanks
+    #  Nr.  St  Länge    Beschreibung
+    #  1   0    4 Zeichen    Länge des Datensatzes, 187 + x * 29 (x..Anzahl Erweiterungsteile)
+    #  2   4    1 Zeichen    Datensatz-Typ, immer 'C'
+    #  3   5    8 Zeichen    Bankleitzahl des Auftraggebers (optional)
+    #  4   13  8 Zeichen    Bankleitzahl des Kunden
+    #  5   21  10 Zeichen  Kontonummer des Kunden
+    #  6   31  13 Zeichen  Verschiedenes
+    #      1. Zeichen: "0"
+    #      2. - 12. Zeichen: interne Kundennummer oder Nullen
+    #      13. Zeichen: "0"
+    #      Die interne Nummer wird vom erstbeauftragten Institut zum endbegünstigten Institut weitergeleitet. Die Weitergabe der internenen Nummer an den Überweisungsempfänger ist der Zahlstelle freigestellt.
+    #  7   44  5 Zeichen  Art der Transaktion (7a: 2 Zeichen, 7b: 3 Zeichen)
+    #      "04000" Lastschrift des Abbuchungsauftragsverfahren
+    #      "05000" Lastschrift des Einzugsermächtigungsverfahren
+    #      "05005" Lastschrift aus Verfügung im elektronischen Cash-System
+    #      "05006" Wie 05005 mit ausländischen Karten
+    #      "51000" Überweisungs-Gutschrift
+    #      "53000" Überweisung Lohn/Gehalt/Rente
+    #      "5400J" Vermögenswirksame Leistung (VL) ohne Sparzulage
+    #      "5400J" Vermögenswirksame Leistung (VL) mit Sparzulage
+    #      "56000" Überweisung öffentlicher Kassen
+    #      Die im Textschlüssel mit J bezeichnete Stelle, wird bei Übernahme in eine Zahlung automatisch mit der jeweils aktuellen Jahresendziffer (7, wenn 97) ersetzt.
+    #  8   49  1 Zeichen  Reserviert, " " (Blank)
+    #  9   50  11 Zeichen  Betrag in DM
+    #  10   61  8 Zeichen  Bankleitzahl des Auftraggebers
+    #  11   69  10 Zeichen  Kontonummer des Auftraggebers
+    #  12   79  11 Zeichen  Betrag in Euro einschließlich Nachkommastellen, nur belegt, wenn Euro als Währung angegeben wurde (A12, C17a), sonst Nullen
+    #  13   90  3 Zeichen  Reserviert, 3 Blanks
+    #  14a 93  27 Zeichen  Name des Kunden
+    #  14b 120  8 Zeichen  Reserviert, 8 Blanks
     #    Insgesamt 128 Zeichen
     #
-    #	15 128  27 Zeichen  Name des Auftraggebers
-    #	16 155  27 Zeichen  Verwendungszweck
-    #	17a 182  1 Zeichen  Währungskennzeichen
-    #			" " = DM
-    #			"1" = Euro
-    #	17b 183  2 Zeichen  Reserviert, 2 Blanks
-    #	18 185  2 Zeichen  Anzahl der Erweiterungsdatensätze, "00" bis "15"
-    #	19 187  2 Zeichen  Typ (1. Erweiterungsdatensatz)
-    #			"01" Name des Kunden
-    #			"02" Verwendungszweck
-    #			"03" Name des Auftraggebers
-    #	20 189  27 Zeichen  Beschreibung gemäß Typ
-    #	21 216  2 Zeichen  wie C19, oder Blanks (2. Erweiterungsdatensatz)
-    #	22 218  27 Zeichen  wie C20, oder Blanks
-    #	23 245  11 Zeichen  11 Blanks
-    #	Insgesamt 256 Zeichen, kann wiederholt werden (max 3 mal)
+    #  15 128  27 Zeichen  Name des Auftraggebers
+    #  16 155  27 Zeichen  Verwendungszweck
+    #  17a 182  1 Zeichen  Währungskennzeichen
+    #      " " = DM
+    #      "1" = Euro
+    #  17b 183  2 Zeichen  Reserviert, 2 Blanks
+    #  18 185  2 Zeichen  Anzahl der Erweiterungsdatensätze, "00" bis "15"
+    #  19 187  2 Zeichen  Typ (1. Erweiterungsdatensatz)
+    #      "01" Name des Kunden
+    #      "02" Verwendungszweck
+    #      "03" Name des Auftraggebers
+    #  20 189  27 Zeichen  Beschreibung gemäß Typ
+    #  21 216  2 Zeichen  wie C19, oder Blanks (2. Erweiterungsdatensatz)
+    #  22 218  27 Zeichen  wie C20, oder Blanks
+    #  23 245  11 Zeichen  11 Blanks
+    #  Insgesamt 256 Zeichen, kann wiederholt werden (max 3 mal)
     # ==== Parameter
     # booking<Object>::Booking object to be written to c-sektion
     # ==== Returns
@@ -255,7 +256,7 @@ module KingDta
       add_ext( exts[6..9] )
       add_ext( exts[10..13] )
       dta_string
-    end	#dataC
+    end  #dataC
 
     # Format an extension so it can be used in string substitution
     # ==== Returns
@@ -272,31 +273,31 @@ module KingDta
     # <Array[Array[String,String]]>:: two dim. ary containing: [extension type(01 02 03),string content]
     def add_ext( ext)
       raise "Nur #{ext.size} Erweiterungstexte, 4 benötigt" if ext.size != 4
-      str =	"%2.2s%-27.27s" % format_ext(ext[0][0], ext[0][1] )
-      str +=	"%2.2s%-27.27s" % format_ext( ext[1][0], ext[1][1] )
-      str +=	"%2.2s%-27.27s" % format_ext( ext[2][0], ext[2][1] )
-      str +=	"%2.2s%-27.27s" % format_ext( ext[3][0], ext[3][1] )
+      str =  "%2.2s%-27.27s" % format_ext(ext[0][0], ext[0][1] )
+      str +=  "%2.2s%-27.27s" % format_ext( ext[1][0], ext[1][1] )
+      str +=  "%2.2s%-27.27s" % format_ext( ext[2][0], ext[2][1] )
+      str +=  "%2.2s%-27.27s" % format_ext( ext[3][0], ext[3][1] )
       # devider 12 blanks
       str += ' ' * 12
       unless  str !~ /\S/ # only add if something is in there .. only whitespace => same as str.blank?
         raise "DTAUS: Längenfehler C/3 #{str.size} " if str.size != 128
         dta_string << str
       end
-    end	#dataC
+    end  #dataC
 
     #Erstellen E-Segment (Prüfsummen) der DTAUS-Datei
     #Aufbau:
-    #	Nr.	Start Länge 	Beschreibung
-    #	1 	0  	4 Zeichen  	Länge des Datensatzes, immer 128 Bytes, also immer "0128"
-    #	2 	4  	1 Zeichen  	Datensatz-Typ, immer 'E'
-    #	3 	5  	5 Zeichen  	"     " (Blanks)
-    #	4 	10  7 Zeichen  	Anzahl der Datensätze vom Typ C
-    #	5 	17  13 Zeichen  Kontrollsumme Beträge
-    #	6 	30  17 Zeichen  Kontrollsumme Kontonummern
-    #	7 	47  17 Zeichen  Kontrollsumme Bankleitzahlen
-    #	8 	64  13 Zeichen  Kontrollsumme Euro, nur belegt, wenn Euro als Währung angegeben wurde (A12, C17a)
-    #	9 	77  51 Zeichen  51 Blanks
-    #	Insgesamt 128 Zeichen
+    #  Nr.  Start Länge   Beschreibung
+    #  1   0    4 Zeichen    Länge des Datensatzes, immer 128 Bytes, also immer "0128"
+    #  2   4    1 Zeichen    Datensatz-Typ, immer 'E'
+    #  3   5    5 Zeichen    "     " (Blanks)
+    #  4   10  7 Zeichen    Anzahl der Datensätze vom Typ C
+    #  5   17  13 Zeichen  Kontrollsumme Beträge
+    #  6   30  17 Zeichen  Kontrollsumme Kontonummern
+    #  7   47  17 Zeichen  Kontrollsumme Bankleitzahlen
+    #  8   64  13 Zeichen  Kontrollsumme Euro, nur belegt, wenn Euro als Währung angegeben wurde (A12, C17a)
+    #  9   77  51 Zeichen  51 Blanks
+    #  Insgesamt 128 Zeichen
     def add_e
       str = '0128'
       str += 'E'
@@ -311,5 +312,5 @@ module KingDta
       dta_string << str
     end
     
-  end	#class dtaus
-end	
+  end  #class dtaus
+end
--- lib/king_dta/helper.rb
+++ lib/king_dta/helper.rb
@@ -1,3 +1,4 @@
+# encoding: utf-8
 module KingDta
   module Helper
     #Zeichen umsetzen gemäss DTA-Norm
@@ -14,20 +15,7 @@ module KingDta
       text.gsub!('ß', 'SS')
       
       text.gsub! /[^a-zA-Z0-9\ \.\,\&\-\/\+\*\$\%]/, '' # Remove all invalid chars
-      
-      text = text.upcase.strip
-      text
-#		text = text.to_s()
-#		puts "Text kein String >#{text}< (#{text.class})" if ! text.kind_of?( String )
-#		text = text.upcase()
-#		text = text.gsub('Ä', 'AE')
-#		text = text.gsub('Ü', 'UE')
-#		text = text.gsub('Ö', 'OE')
-#		text = text.gsub('ä', 'AE')
-#		text = text.gsub('ü', 'UE')
-#		text = text.gsub('ö', 'OE')
-#		text = text.gsub('ß', 'SS')
-#		return text = text.strip
+      text.upcase.strip
     end
   end
 end
