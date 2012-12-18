b8226183ba60506203ffaae8910dd9f3082dfcd3
Karl Grogan <krlgrgn@gmail.com>
2012-12-18 15:36:54 +0000

    Fix bug when checking whether to raise an exception on text length or not.

--- lib/king_dta/dtazv_booking.rb
+++ lib/king_dta/dtazv_booking.rb
@@ -4,13 +4,12 @@ module KingDta
   class DtazvBooking < Booking
     attr_accessor :customer_bill_text
 
-    def initialize( account, value, text=nil, account_key=nil )
-      super(account, value, text, account_key)
-      raise Exception.new("The length of your text is too long. It must be at most 140 chars long.") if t.length > 140
+    def initialize( account, value, account_key=nil )
+      super(account, value, nil, account_key)
     end
 
     def text=(text)
-      raise Exception.new("The length of your text is too long. It must be at most 140 chars long.") if t.length > 140
+      raise Exception.new("The length of your text is too long. It must be at most 140 chars long.") if text.length > 140
       @text = text
     end
 
