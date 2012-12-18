115df581612bc12049c0d6f1252659f310e925d3
Karl Grogan <krlgrgn@gmail.com>
2012-12-18 15:38:42 +0000

    Check text array for funny chars in DTAUS booking.

--- lib/king_dta/dtaus_booking.rb
+++ lib/king_dta/dtaus_booking.rb
@@ -6,15 +6,14 @@ module KingDta
     def initialize( account, value, text=[], account_key=nil )
       super(account, value, text, account_key)
 
-      #@text = text.each{ |t| 
-      #  raise Exception.new("The length of your text is too long. It must be at most 27 chars long.") if t.size > 128
-      #  convert_text(t)
-      #}
+      @text = text.each{ |t| 
+        raise Exception.new("The length of your text is too long. It must be at most 27 chars long.") if t.size > 128
+        convert_text(t)
+      }
     end
 
     def text=(text)
        @text = text.each { |t| 
-         # TODO: Add some configuration because this should be specific to DTAUS but it is taken into consideration for DTAZV
          raise Exception.new("The length of your text is too long. It must be at most 27 chars long.") if t.size > 128
          convert_text( t ) 
        }
