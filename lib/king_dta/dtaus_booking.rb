658c46416ae7715dd491ddb259bb2c291975dc4c
Karl Grogan <krlgrgn@gmail.com>
2012-12-19 09:57:57 +0000

    Refactor and change char length check.

--- lib/king_dta/dtaus_booking.rb
+++ lib/king_dta/dtaus_booking.rb
@@ -3,20 +3,17 @@
 module KingDta
   class DtausBooking < Booking
 
-    def initialize( account, value, text=[], account_key=nil )
-      super(account, value, text, account_key)
-
-      @text = text.each{ |t| 
-        raise Exception.new("The length of your text is too long. It must be at most 27 chars long.") if t.size > 128
-        convert_text(t)
-      }
+    def initialize( account, value, account_key=nil )
+      super(account, value, nil, account_key)
     end
 
     def text=(text)
-       @text = text.each { |t| 
-         raise Exception.new("The length of your text is too long. It must be at most 27 chars long.") if t.size > 128
-         convert_text( t ) 
-       }
+      raise Exception.new("The text parameter must be passed as an array") if !text.kind_of?(Array)
+      
+      @text = text.each{ |t| 
+        raise Exception.new("The length of your text is too long. It must be at most 27 chars long.") if t.size > 27
+        convert_text(t)
+      }
     end
 
   end
