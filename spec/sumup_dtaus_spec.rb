9a6e4fff283916f85d289b190c3b08fcc9731d79
Karl Grogan <krlgrgn@gmail.com>
2012-12-19 15:14:15 +0000

    Update sumup_dtaus_spec with test for owner name extension.

--- spec/sumup_dtaus_spec.rb
+++ spec/sumup_dtaus_spec.rb
@@ -2,7 +2,7 @@
 
 require 'spec_helper'
 
-describe KingDta:: SumupDtaus do
+describe KingDta::SumupDtaus do
 
   before :each do
     @sumup_dtaus = KingDta::SumupDtaus.new('GK')
@@ -34,6 +34,19 @@ describe KingDta:: SumupDtaus do
     str.should include(@kto2.owner_name.upcase)
     out =  "0216C00000000370400440002787777000000000000051000 0000000000037040044782897003700000022025   PETER & MAY GMBH                   GIMME YOUR MONEY AG        DEFAULT VERWENDUNGSZWECK   1  0102                                                                   "
     str.should == out
+  end
+
+  it "should create c-sektion with long account owner name in extension" do
+    @sumup_dtaus.account = KingDta::Account.new(:bank_account_number => @kto1.bank_account_number, :bank_number => @kto1.bank_number, 
+                                          :owner_name =>  'A very long name exeeding 27 characters even longer 54 chars', :bank_name => @kto1.bank_name)
+
+    @sumup_dtaus.add(@booking)
+    @sumup_dtaus.bookings.first.text = ['SalesKing Monatsbeitrag ','08/10 Freelancer Version']
+    @sumup_dtaus.add_c(@booking)
+    str = @sumup_dtaus.dta_string
+    str.length.should == 256
+    str.should include(@kto2.owner_name.upcase)
+    out = "0245C00000000370400440002787777000000000000005000 0000000000037040044782897003700000022025   PETER & MAY GMBH                   A VERY LONG NAME EXEEDING 2SALESKING MONATSBEITRAG 08/1  020210 FREELANCER VERSION      037 CHARACTERS EVEN LONGER 54           "
     str.should == out
   end
 
