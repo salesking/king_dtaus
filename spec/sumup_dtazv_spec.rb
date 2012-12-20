9f8c3e36fce61aceee9ada47cd578911d0f2c572
Karl Grogan <krlgrgn@gmail.com>
2012-12-20 11:35:23 +0000

    Add unit test to test the T block of a GB dtazv file.

--- spec/sumup_dtazv_spec.rb
+++ spec/sumup_dtazv_spec.rb
@@ -14,10 +14,12 @@ describe KingDta::SumupDtazv do
     @sumup_dtazv.add_t(@booking).size.should == 768
   end
 
-  it "should create a T-block with customer bill text in it" do
+  it "should create a T-block with customer bill text in it in EUR" do
+    sumup_dtazv = KingDta::SumupDtazv.new('EUR', Date.parse('2011-08-28'))
+    sumup_dtazv.account = KingDta::Account.new sender_opts
     @booking.customer_bill_text = "A"*27
-    @sumup_dtazv.add_t(@booking)
-    str = @sumup_dtazv.dta_string
+    sumup_dtazv.add_t(@booking)
+    str = sumup_dtazv.dta_string
     out = "0768T37050299EUR132604963411082800000000   0000000000COBADEFF366                                                                                                                                               DE DALAI LAMA                                                            BUSH-AVENUE 55                     445555 INDIA                                                                                             /PL10105013151000002308622378      EUR00000000000220250                                                                                                                                            00000000                         0013AAAAAAAAAAAAAAAAAAAAAAAAAAA                                   0                                                   00"
     str.should == out
   end
@@ -39,5 +41,15 @@ describe KingDta::SumupDtazv do
   it "should not allow assignment to payment_type instance variable directly" do
       sumup_dtazv = KingDta::SumupDtazv.new('GBP')
       lambda { sumup_dtazv.payment_type = 19 }.should raise_error(NoMethodError)
+  end
+
+  it "should create a T-block with customer bill text in it in EUR" do
+    sumup_dtazv = KingDta::SumupDtazv.new('GBP', Date.parse('2011-08-28'))
+    sumup_dtazv.account = KingDta::Account.new sender_opts
+    @booking.customer_bill_text = "A"*27
+    sumup_dtazv.add_t(@booking)
+    str = sumup_dtazv.dta_string
+    out = "0768T37050299GBP132604963411082800000000   0000000000COBADEFF366                                                                                                                                               DE DALAI LAMA                                                            BUSH-AVENUE 55                     445555 INDIA                                                                                             /PL10105013151000002308622378      GBP00000000000220250                                                                                                                                            00000000                         0000AAAAAAAAAAAAAAAAAAAAAAAAAAA                                   0                                                   00"
+    str.should == out
   end
 end
