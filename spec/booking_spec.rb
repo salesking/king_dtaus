33932ddd0c6a93c4190b97b1382be0e717cfda30
jethroo <cwirth79@web.de>
2013-10-30 18:18:29 +0100

    added test for not tested currency size exception

--- spec/booking_spec.rb
+++ spec/booking_spec.rb
@@ -25,6 +25,14 @@ describe KingDta::Booking do
     lambda{ KingDta::Booking.new(@account, 0.00) }.should raise_error(KingDta::Exception)
   end
 
+  it "should raise if currency size is greater than 3" do
+    lambda{ KingDta::Booking.new(@account, 15973, nil, nil, "EURO") }.should raise_error(KingDta::Exception,/currency code needs to be 3 chars. You gave me: .*/)
+  end
+
+  it "should raise if currency size is less than 3" do
+    lambda{ KingDta::Booking.new(@account, 15973, nil, nil, "EU") }.should raise_error(KingDta::Exception,/currency code needs to be 3 chars. You gave me: .*/)
+  end
+
   it "should set pos to false with negative value" do
     b = KingDta::Booking.new(@account, -1)
     b.value.should == 100
